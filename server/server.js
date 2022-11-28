dc00105a20fa08c852b0ea1f4861ff663333642c
Kyle <kylevance801@gmail.com>
2022-11-28 15:57:13 -0700

    heroku issues contd

--- server/server.js
+++ server/server.js
@@ -39,8 +39,4 @@ db.once('open', () => {
   })
 });
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
 startApollo(typeDefs, resolvers);
