3232033e2dd9643b5c9ad43d76550ee817f9db38
Kyle <kylevance801@gmail.com>
2022-11-28 13:10:50 -0700

    added apollo links to app.js, authLink, createHTTPLink, and client

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,31 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const createHttpLink = createHttpLink({
+  url: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => { 
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(createHttpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
+    <ApolloProvider client={client}>
     <Router>
       <>
         <Navbar />
@@ -25,6 +47,7 @@ function App() {
         </Routes>
       </>
     </Router>
+    </ApolloProvider>
   );
 }
 
