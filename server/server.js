f3c025fdf80c6569a02736735eee072796dc5fac
Kyle <kylevance801@gmail.com>
2022-11-28 17:49:56 -0700

    home as /

--- server/server.js
+++ server/server.js
@@ -21,11 +21,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
-
 
 const startApollo = async (typeDefs, resolvers) => {
   await server.start();
